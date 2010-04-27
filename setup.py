bf0e2817d346e591453b3de6694c7965801bdcf1
ckreutzer <herr.kreutzer@gmail.com>
2010-04-27 21:33:29 +0200

    Completed setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python
 
+import os
+
 from distutils.core import setup
+from tvrage import __version__, __author__, __license__
 
 setup(name='python-tvrage',
-      version='0.1.4',
       description='python client for the tvrage.com XML API',
-      author='Christian Kreutzer',
+      long_description = file(
+          os.path.join(os.path.dirname(__file__), 'README')).read(),
+      license=__license__,
+      version=__version__,
+      author=__author__,
       author_email='herr.kreutzer@gmail.com',
-      license='BSD License',
       url='http://bitbucket.org/ckreutzer/python-tvrage/',
       packages=['tvrage'],
+      classifiers = [
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Programming Language :: Python',
+          'Operating System :: OS Independent'
+      ]
      )
 
