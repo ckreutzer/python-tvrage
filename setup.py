742b7df42e65154ff28ca914b1ff302217293648
ckreutzer <herr.kreutzer@gmail.com>
2012-09-15 18:21:57 +0200

    fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='python-tvrage',
       author=__author__,
       author_email='herr.kreutzer@gmail.com',
       # url='http://bitbucket.org/ckreutzer/python-tvrage/',
-      url='https://github.com/ckreutzer/python-tvrage'
+      url='https://github.com/ckreutzer/python-tvrage',
       packages=['tvrage'],
       classifiers = [
           'Development Status :: 4 - Beta',
